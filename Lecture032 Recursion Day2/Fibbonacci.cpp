5391a786ad30254840b94867f9b8838c9d21c7a5
Hemant Prajapati <83368799+HemantPra389@users.noreply.github.com>
2022-10-20 17:30:47 +0530

    Update Fibbonacci.cpp
    Recursion Function for Fibonacci function is not coded properly. So I re-coded the full code for cpp

--- Lecture032 Recursion Day2/Fibbonacci.cpp
+++ Lecture032 Recursion Day2/Fibbonacci.cpp
@@ -1,11 +1,17 @@
-#include<iostream> 
+#include<iostream>
 using namespace std;
-
-
-int main() {
-
-
-
-
+int fib(int n){
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    int ans = fib(n-1)+fib(n-2);
+    return ans;
+    
+}
+int main(){
+    cout<<"Fib Sum is: "<<fib(10)<<endl;
     return 0;
 }
